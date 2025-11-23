1c04271d8454afd4df2f52a8c728952bd8aa277a
Bryce Tyre <bryc11499@gmail.com>
2025-11-22 21:59:42 -0700

    Added main to INSTALLED_APPS

--- portfolio_site/portfolio_site/settings.py
+++ portfolio_site/portfolio_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'   # added my app here so Django knows about it
 ]
 
 MIDDLEWARE = [
