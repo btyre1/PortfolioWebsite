f0fc7b9d002c1246d809a7fbeff0bfb7fb617e55
Bryce Tyre <bryc11499@gmail.com>
2025-11-27 23:48:08 -0700

    Added model classes & functions

--- portfolio_site/main/models.py
+++ portfolio_site/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    level = models.IntegerField(default=0)  
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    short_desc = models.CharField(max_length=300)
+    long_desc = models.TextField()
+    tech_stack = models.CharField(max_length=300, blank=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+    tags = models.CharField(max_length=200, blank=True)  # simple CSV tags
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='projects/')
+    caption = models.CharField(max_length=200, blank=True)
+
