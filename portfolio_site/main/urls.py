1dd462500257a238365d8b754fce645e756ac129
Bryce Tyre <bryc11499@gmail.com>
2025-11-28 00:03:03 -0700

    Polished comments & added MEDIA_URL routing

--- portfolio_site/main/urls.py
+++ portfolio_site/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from . import views  # lets me connect URLs to my view functions
 
 urlpatterns = [
-    path("", views.home, name="home"), # Home page
+    path("", views.home, name="home"), # This connects the homepage URL to the home view
 ]
