2a5f720cc2903642641f870b4e69f1644fe3dad3
Bryce Tyre <bryc11499@gmail.com>
2025-11-28 22:47:01 -0700

    Added load static to base.html & fixed path in extends

--- portfolio_site/main/models.py
+++ portfolio_site/main/models.py
@@ -27,4 +27,3 @@ class ProjectImage(models.Model):
     project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='projects/')
     caption = models.CharField(max_length=200, blank=True)
-
