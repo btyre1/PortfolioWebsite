c476ed1338b6956be90f4aa4fe241cd4c8ab2f9c
Bryce Tyre <bryc11499@gmail.com>
2025-11-28 18:39:48 -0700

    Registered my models & created admin user

--- portfolio_site/main/admin.py
+++ portfolio_site/main/admin.py
@@ -1,5 +1,11 @@
 # portfolio_site/main/admin.py
 
 from django.contrib import admin
+from .models import Project, ProjectImage, Skill
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(ProjectImage)
+admin.site.register(Skill)
+
+
